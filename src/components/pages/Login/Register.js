8195221a1f6f494e972b4d4be286a7913cf56a87
unknown <labib.salimi30@gmail.com>
2022-04-30 09:53:03 +0600

    user registration added

--- src/components/pages/Login/Register.js
+++ src/components/pages/Login/Register.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const handleCreateUserWithEmailAndPassword = async (event) => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+    };
+
     return (
         <div className='container mx-auto'>
             <div className='w-1/2 mx-auto'>
                 <h3 className='text-3xl font-semibold mb-5 text-left'>Please Register</h3>
-                <form className='flex flex-col text-xl'>
-                    <input className='p-3 mb-3 rounded-md' type='text' placeholder='Name' required></input>
-                    <input className='p-3 mb-3 rounded-md' type='email' placeholder='Email' required></input>
-                    <input className='p-3 mb-3 rounded-md' type='password' placeholder='Password' required></input>
-                    <p className='mb-3'>Already have an account? <Link to='/login' className='text-blue-400'>Login</Link>.</p>
-                    <input className='w-2/3 mx-auto text-white bg-blue-400 hover:bg-blue-500 rounded-md px-3 py-1 mb-3' type='submit' value='Register' />
+                <form
+                    onSubmit={handleCreateUserWithEmailAndPassword}
+                    className='flex flex-col text-xl'
+                >
+                    <input
+                        className='p-3 mb-3 rounded-md'
+                        type='text'
+                        placeholder='Name'
+                        ref={nameRef}
+                        required
+                    ></input>
+                    <input
+                        className='p-3 mb-3 rounded-md'
+                        type='email'
+                        placeholder='Email'
+                        ref={emailRef}
+                        required
+                    ></input>
+                    <input
+                        className='p-3 mb-3 rounded-md'
+                        type='password'
+                        placeholder='Password'
+                        ref={passwordRef}
+                        required
+                    ></input>
+                    <p
+                        className='mb-3'>Already have an account?
+                        <Link
+                            to='/login'
+                            className='text-blue-400'
+                        >Login</Link>.
+                    </p>
+                    <input
+                        className='w-2/3 mx-auto text-white bg-blue-400 hover:bg-blue-500 rounded-md px-3 py-1 mb-3'
+                        type='submit'
+                        value='Register'
+                    ></input>
                 </form>
             </div>
         </div>
